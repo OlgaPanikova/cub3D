73923192fe2da72acf31867879681b53eb0dbeaa
lordoftrade <lordoftrade@rambler.ru>
2024-10-04 18:54:00 +0400

    norma in animal_bonus

--- srcb/raycasting_b/animal_bonus.c
+++ srcb/raycasting_b/animal_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   animal_bonus.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: opanikov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mgreshne <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 17:01:37 by opanikov          #+#    #+#             */
-/*   Updated: 2024/10/04 18:43:19 by opanikov         ###   ########.fr       */
+/*   Updated: 2024/10/04 18:53:22 by mgreshne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 
 void	load_player_sprites(t_cub *data)
 {
-	data->sprites[0] = mlx_xpm_file_to_image(data->mlx_ptr, data->hand1, &data->sprite_width, &data->sprite_height);
-	data->sprites[1] = mlx_xpm_file_to_image(data->mlx_ptr, data->hand2, &data->sprite_width, &data->sprite_height);
+	data->sprites[0] = mlx_xpm_file_to_image(data->mlx_ptr, data->hand1,
+			&data->sprite_width, &data->sprite_height);
+	data->sprites[1] = mlx_xpm_file_to_image(data->mlx_ptr, data->hand2,
+			&data->sprite_width, &data->sprite_height);
 	if (!data->sprites[0] || !data->sprites[1])
 		ft_exit(data, "Error loading player sprites", 1);
 }
@@ -41,12 +43,11 @@ void	update_player_animation(t_cub *data)
 void	render_player(t_cub *data)
 {
 	void	*current_sprite;
-	int	x;
-	int	y;
+	int		x;
+	int		y;
 
 	x = 580;
 	y = 557;
-
 	current_sprite = data->sprites[data->sprite_index];
 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, current_sprite, x, y);
 }
